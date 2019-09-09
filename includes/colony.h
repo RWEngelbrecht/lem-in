4e7ef0267e2a1301fa9ae697cbfef2ba5f6c3456
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2019-09-09 09:47:23 +0200

    started struct for rooms might not be final

--- includes/colony.h
+++ includes/colony.h
@@ -3,11 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   colony.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrheeder <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:35:08 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/09/05 11:35:09 by jrheeder         ###   ########.fr       */
+/*   Updated: 2019/09/09 09:39:59 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LEM_IN_H
+# define LEM_IN_H
 
+#include "libft/libft.h"
+
+typedef struct		s_room
+{
+	int				start;
+	int				end;
+	char			*name;
+	int				x;
+	int				y;
+	int				ants;
+	struct s_room	*next;
+}					t_room;
+
+#endif
