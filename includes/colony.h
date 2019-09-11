4c147e225812da5b4ba285c786045fe4422f527a
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2019-09-11 12:25:53 +0200

    changed spaces to tabs

--- includes/colony.h
+++ includes/colony.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   colony.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:35:08 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/09/10 15:38:48 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/09/11 11:22:41 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,6 @@ typedef struct		s_log
 
 char	*get_room_name(char *line);
 t_log	*read_input(char **line);
-int     check_name(t_log *data/*, int phase*/);
+int		check_name(t_log *data/*, int phase*/);
 
 #endif
