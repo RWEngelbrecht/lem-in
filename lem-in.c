7e514d3dd1ccf7f0ed8d23278086e49d797cf207
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2019-09-11 08:17:42 +0200

    printing all rooms and links from t_log struct

--- lem-in.c
+++ lem-in.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lem-in.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:28:51 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/09/10 15:38:57 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/09/11 08:11:14 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,22 @@ int main()
 {
 	char *line;
 	t_log *ret;
+	int i = 0;
+	int j = 0;
 
 	line = NULL;
 	ret = NULL;
 	ret = read_input(&line);
-	//printf("ant amnt = %d\nstart_line = %d\nend_line = %d", ret->ant_amnt, ret->ant_amnt, ret->end_line);
+
+	printf("ant amnt = %d\nstart_line = %d\nend_line = %d\n", ret->ant_amnt, ret->start_line, ret->end_line);
+	while (ret->rooms[i])
+	{
+		printf("rooms[%d] = %s\n", i, ret->rooms[i]);
+		i++;
+	}
+	while (ret->links[j])
+	{
+		printf("links[%d] = %s\n", j, ret->links[j]);
+		j++;
+	}
 }
