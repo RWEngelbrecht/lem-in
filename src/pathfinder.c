a2bcdd5ff83deae97fab0312907b2753d21bddc5
jordanrheeder <jordanrheeder@gmail.com>
2019-09-17 10:16:27 +0200

    New maze function being updated to allocate temp... debug time

--- src/pathfinder.c
+++ src/pathfinder.c
@@ -14,7 +14,7 @@
 #include "../libft/libft.h"
 #include <stdio.h>
 //finds every link in all_links that references name, returns it in 2d array
-char	**find_linked_rooms(char *name, char **all_links)
+char	**find_linked_rooms(t_str name, t_str *all_links)
 {
 	unsigned int	name_len;
 	unsigned int	i;
@@ -40,7 +40,7 @@ printf("connected[%d] = %s\nall links[%d] = %s\n", j, connected[j], i, all_links
 
 // void	add_room_links(t_room *maze)
 
-int		get_coordinate(char *room, char type)
+int		get_coordinate(t_str room, char type)
 {
 	int		i;
 	int		j;
@@ -61,18 +61,50 @@ int		get_coordinate(char *room, char type)
 //Takes current maze, name + coordinates of room to add and 2D array
 //with refs to all rooms it links to as well as
 //type (i.e. start(0), end(2) or normal(1))
-t_room	*new_maze(t_str *rooms, unsigned int type)
+t_room	*new_maze(t_str room, unsigned int type, t_log *data, t_str *cur_links)
 {
-	t_room	*maze;
+	t_room	**maze;
+	t_room	*temp;
+	int i;
+	int j;
+	int k;
+	char **link_name;
 
+	k = 0;
 	maze = malloc(sizeof(t_room));
-	maze->room_type = type;
-	maze->name = get_room_name(rooms);
-	maze->x = get_coordinate(rooms, 'x');
-	maze->y = get_coordinate(rooms, 'y');
-	maze->visited = 0;
-	maze->room_links = NULL;
-	return (maze);
+	(*maze)->room_type = type;
+	(*maze)->name = ft_strdup(room);
+	(*maze)->x = get_coordinate(room, 'x');
+	(*maze)->y = get_coordinate(room, 'y');
+	(*maze)->visited = 0;
+	while (cur_links[k])
+	{
+		temp = malloc(sizeof(t_room));
+		i = 0;
+		j = 0;
+		link_name = ft_strsplit(cur_links[j], '-');
+		while (data->rooms[i])
+		{
+			if (ft_strequ(link_name[1], get_room_name(data->rooms[i])))
+				break;
+			i++;
+		}
+		if (i == data->start_index)
+			temp->room_type = 0;
+		else if (i == data->end_index)
+			temp->room_type = 1;
+		else
+			temp->room_type = 2;
+		temp->name = ft_strdup(*link_name);
+		temp->x = get_coordinate(data->rooms[i], 'x');
+		temp->y = get_coordinate(data->rooms[i], 'y');
+		temp->visited = 0;
+		temp->room_links = NULL;
+		(*maze)->room_links[j++] = temp;
+		k++;
+		maze = &temp;
+	}
+	return (*maze);
 }
 
 t_room	*connect_links(t_log *data)
@@ -83,8 +115,6 @@ t_room	*connect_links(t_log *data)
 	char *room_name;
 	char *start_room;
 	char *end_room;
-
-// int k = 0, m = 0;
 
 	maze = NULL;
 	i = 0;
@@ -97,11 +127,11 @@ t_room	*connect_links(t_log *data)
 printf("%d: room_name = %s\n", i, room_name);
 		cur_links = find_linked_rooms(room_name, data->links);
 		if (i == data->start_index)
-			maze = new_maze(data->rooms, 0);
+			maze = new_maze(data->rooms[i], 0, data, cur_links);
 		else if (i == data->end_index)
-			maze = new_maze(data->rooms, 2);
+			maze = new_maze(data->rooms[i], 1, data, cur_links);
 		else
-			maze = new_maze(data->rooms, 1);
+			maze = new_maze(data->rooms[i], 2, data, cur_links);
 		i++;
 	}
 	return (maze);
