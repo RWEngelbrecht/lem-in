31ade3433e417a9cb5c1703836c8d1c6d5984ba9
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-09-26 06:32:11 +0200

    some printfs for testing room linking

--- lem-in.c
+++ lem-in.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lem-in.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:28:51 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/09/19 14:46:32 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/09/22 18:06:13 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int main()
 	char *line;
 	t_log *ret;
 	t_room *maze;
-	// int i = 0;
-	// int j = 0;
+//	int i = 0;
+//	 int j = 0;
 //	int valid;
 
 	line = NULL;
@@ -26,6 +26,20 @@ printf("reading input\n");
 	ret = read_input(line);				//*****DOES NOT LEAK****
 printf("input read\n");
 printf("validating input\n");
+	validate(ret);						//******LEAKS A F##K TON****
+printf("input validated\n");
+printf("mapping input\n");
+	maze = init_rooms(ret);				//*****SOMEWHAT LEAKY******
+printf("input initialized\n");
+//while (maze)
+//{
+	// while (maze->room_links[i])
+	// {
+//		printf("room(%d): type = %d\nname = %s\nXY = %d %d\nvisited = %d\n",i, maze->room_type, maze->name, maze->x, maze->y, maze->visited);
+//		i++;
+	// }
+//	maze = maze->next;
+//}
 // while (ret->rooms[i])
 // {
 // 	printf("rooms[%d] = %s\n", i, ret->rooms[i]);
@@ -36,11 +50,6 @@ printf("validating input\n");
 // 	printf("links[%d] = %s\n", j, ret->links[j]);
 // 	j++;
 // }
-	validate(ret);						//******LEAKS A F##K TON****
-printf("input validated\n");
-printf("mapping input\n");
-	maze = init_rooms(ret);				//*****SOMEWHAT LEAKY******
-printf("input mapped\n");
 	//valid = check_name_XY(ret);
 //printf("VALID CHECK: %d\n", check_links(ret));
 //	printf("ant amnt = %d\nstart_line = %d\nend_line = %d\n", ret->ant_amnt, ret->start_line, ret->end_line);
