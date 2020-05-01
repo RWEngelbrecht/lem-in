062e94808446dae08f4edc191f83f304f61a148d
Henricus Wouter de Vos <45897219+Hallocoos@users.noreply.github.com>
2020-05-01 13:26:43 +0200

    Revert "Lem in refactor rigardt" (#5)

--- libft/ft_only_digits.c
+++ libft/ft_only_digits.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_only_digits.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/25 11:43:52 by rengelbr          #+#    #+#             */
-/*   Updated: 2020/05/01 11:30:46 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/09/11 08:27:44 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int		ft_only_digits(char *str)
 {
 	while (*str)
 	{
+		if (*str == '-')
+			str++;
 		if (!ft_isdigit(*str))
 			return (0);
 		str++;
