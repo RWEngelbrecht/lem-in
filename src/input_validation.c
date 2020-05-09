38963135cae4af1c46970e10b7a1cdfeebba05e1
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-05-09 12:10:44 +0200

    fixed leaks caused by is_room(). also checks for L at start of room name

--- src/input_validation.c
+++ src/input_validation.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 11:03:12 by rengelbr          #+#    #+#             */
-/*   Updated: 2020/05/01 13:58:13 by rengelbr         ###   ########.fr       */
+/*   Updated: 2020/05/09 12:03:38 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,17 @@ int		is_comment(char *line)
 // Checking whether input is a room. By checking if wordcount is three.
 int		is_room(char *line)
 {
-	// Need to check that room name doesn't start with a 'L'
 	char** split_room;
 	if (ft_wordcount(line, ' ') == 3)
 	{
 		split_room = ft_strsplit(line, ' ');
-		if (ft_only_digits(split_room[1]) && ft_only_digits(split_room[2]))
+		if (ft_only_digits(split_room[1]) && ft_only_digits(split_room[2])
+			 && !ft_strequ(split_room[0][0], "L") && !ft_strequ(split_room[0][0], "l"))
+		{
+			ft_free_two_d_arr((void**)split_room);
 			return (1);
+		}
+		ft_free_two_d_arr((void**)split_room);
 	}
 	return (0);
 }
