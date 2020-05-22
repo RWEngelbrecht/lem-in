18ff93c7c192ecca0ced1644d3c533f93a8f2883
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-05-22 16:44:37 +0200

    cleaned up a bit

--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:28:51 by jrheeder          #+#    #+#             */
-/*   Updated: 2020/05/21 16:18:09 by rengelbr         ###   ########.fr       */
+/*   Updated: 2020/05/22 16:43:29 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,14 @@ int main()
 	raw_data = read_input(line);
 	validate_file(raw_data);
 	node_array = create_node_array(raw_data);
-	print_map(node_array);
-	// printf("====================\nStarting Algorithm\n");
+	// print_map(node_array);
 	the_path = algo(node_array);
-	t_path *temp = the_path;
-	while (temp != NULL) {
-		printf("%s->", temp->room_name);
-		temp = temp->next;
-	}
-	printf("NULL\n");
-	// printf("Algorithm Finished\n====================\n");
+	// t_path *temp = the_path;
+	// while (temp != NULL) {
+	// 	printf("%s->\n", temp->room_name);
+	// 	temp = temp->next;
+	// }
+	// printf("NULL\n");
 	// free_map(node_array);
+	return (0);
 }
